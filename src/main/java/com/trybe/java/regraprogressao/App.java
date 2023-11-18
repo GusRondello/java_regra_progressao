be2697eda90a2c495120e5073c29b0b7422d55a6
Gustavo Rondello <gustavorondello99@outlook.com>
2023-11-18 16:53:42 -0300

    feat: interface de nome e peso das atividades

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -1,13 +1,40 @@
 package com.trybe.java.regraprogressao;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
  * App.
  */
 public class App {
 
   /**
-   * Metodo main.
+   * Método main.
    */
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in).useDelimiter("\n");
+
+    System.out.println("Digite a quantidade de atividades para cadastrar:");
+    String inputAtividadesQtd = scanner.next();
+
+    int totalAtividades = Integer.parseInt(inputAtividadesQtd);
+
+    String[] nomeAtividades = new String[totalAtividades];
+    int[] pesoAtividades = new int[totalAtividades];
+
+    for (int i = 0; i < totalAtividades; i++) {
+      System.out.println("Digite o nome da atividade " + (i + 1) + ":");
+      String nomeAtividade = scanner.next();
+
+      System.out.println("Digite o peso da atividade " + (i + 1) + ":");
+      String inputPeso = scanner.next();
+
+      int peso = Integer.parseInt(inputPeso);
+      nomeAtividades[i] = nomeAtividade;
+      pesoAtividades[i] = peso;
+    }
+    System.out.println(Arrays.toString(nomeAtividades));
+    System.out.println(Arrays.toString(pesoAtividades));
+    scanner.close();
   }
 }
