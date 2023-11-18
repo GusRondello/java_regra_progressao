3725bdc75481afc104943210be91489bb5d11064
Gustavo Rondello <gustavorondello99@outlook.com>
2023-11-18 17:04:52 -0300

    feat: checagem de peso das atividades

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -21,6 +21,8 @@ public class App {
 
     String[] nomeAtividades = new String[totalAtividades];
     int[] pesoAtividades = new int[totalAtividades];
+    int[] notaAtividades = new int[totalAtividades];
+    int somaPeso = 0;
 
     for (int i = 0; i < totalAtividades; i++) {
       System.out.println("Digite o nome da atividade " + (i + 1) + ":");
@@ -29,10 +31,20 @@ public class App {
       System.out.println("Digite o peso da atividade " + (i + 1) + ":");
       String inputPeso = scanner.next();
 
+      System.out.println("Digite a nota obtida para " + nomeAtividade + ":");
+      String inputNota = scanner.next();
+
       int peso = Integer.parseInt(inputPeso);
+      int nota = Integer.parseInt(inputNota);
       nomeAtividades[i] = nomeAtividade;
       pesoAtividades[i] = peso;
+      notaAtividades[i] = nota;
+      somaPeso += pesoAtividades[i];
     }
+    if (somaPeso != 100) {
+      System.out.println("A soma dos pesos é diferente de 100!");
+    }
+
     System.out.println(Arrays.toString(nomeAtividades));
     System.out.println(Arrays.toString(pesoAtividades));
     scanner.close();
